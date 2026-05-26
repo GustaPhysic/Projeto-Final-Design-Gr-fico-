a54f165e6ab6bf1a34b3a35a60955061817ecf07
Gustavo Rodrigues Santos <1051618@senacminas.edu.br>
2026-05-26 10:55:45 -0300

    Projeto Final Design Gráfico

---
+++ login.js
@@ -0,0 +1,16 @@
+ function login(){
+
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+
+    const usuariovalido = 'admin';
+    const senhavalida = '12345';
+
+    if(user === usuariovalido && pass === senhavalida){
+        localStorage.setItem('auth', 'true');
+        window.location.href = 'brasil.html';
+    } else{
+        document.getElementById('msgerror').style.display = 'block';
+    }
+ }
